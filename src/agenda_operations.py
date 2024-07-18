8cd3fd3d957fb2afa0b72aba6b607fb15d4b3e8a
Silvano Quarto <silvano.quarto@gmail.com>
2024-07-18 18:07:48 +0200

    docstring updated

--- src/agenda_operations.py
+++ src/agenda_operations.py
@@ -18,10 +18,12 @@ class Operations:
 
         value = input(f"Enter new contact's {field}:")
 
+        # Check validity for phone number
         if field == 'phone':
             while not all(val in '0123456789+-*#' for val in value):
                 value = input(f"Enter a new correct contact's {field} with '0123456789+-*#':")
 
+        # Check validity for name and phone number, checking also if client wants to modify an existing contact
         if field in ['name', 'phone'] and value in file[field]:
             mod = input("Name already exists, would you like to update the contact? (Yes, No)")
             if mod.lower() == "yes":
@@ -33,6 +35,8 @@ class Operations:
                     if value.lower() == 'esc':
                         # function to terminate operation (is it necessary?)
                         pass
+
+        # Check validity for email
         if field == 'email':
             while (len(value.split("@")) != 2 or not is_tld(value.split(".")[-1])) and value != "":
                 value = input(f"Enter new contact's {field} with only one @ and appropriate TLD domain:")
@@ -42,11 +46,18 @@ class Operations:
         return value
 
     def add_contact(self):
+
+        """
+        This method opens the file from file_path in read mode.
+        It iterates and asks the client to insert the correct value for each field.
+        It opens the file in write mode and updates the json file.
+        """
         
         # Take the json file in read option
         with open(self.file_path, "r") as agenda_file:
             agenda = json.load(agenda_file)
 
+        # Iteration and appending new contact with correct values
         for field in list(agenda.keys()):
             if field == 'address':
                 for position in list(agenda[field].keys()):
@@ -54,5 +65,6 @@ class Operations:
                 continue
             agenda[field].append(self.add_and_check_info(agenda, field))
 
+        # Update the json file opening it in write option
         with open(self.file_path, "w") as agenda_file:
             json.dump(agenda, agenda_file, indent=4)
