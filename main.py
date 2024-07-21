4ba470d906817542b0eaacfbf42b6c09f2169833
Silvano Quarto <silvano.quarto@gmail.com>
2024-07-21 10:09:04 +0200

    main structure uploaded

---
+++ main.py
@@ -0,0 +1,16 @@
+def menu():
+    print("\nManage your phone agenda:")
+    print("1. Add Contact")
+    print("2. View Contact")
+    print("3. Edit Contact")
+    print("4. Delete Contact")
+    print("5. Search Contact")
+    print("6. Save Agenda")
+    print("7. Load Agenda")
+    print("8. Exit")
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
