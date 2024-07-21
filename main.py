72a88af2fcb634d9aa438ed54ce3c0c90d19f486
Silvano Quarto <silvano.quarto@gmail.com>
2024-07-21 20:08:43 +0200

    main updated

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-def menu():
-    print("\nManage your phone agenda:")
-    print("1. Add Contact")
-    print("2. View Contact")
-    print("3. Edit Contact")
-    print("4. Delete Contact")
-    print("5. Search Contact")
-    print("6. Save Agenda")
-    print("7. Load Agenda")
-    print("8. Exit")
+import src.agenda_operations
 
 def main():
-    pass
+    operations = src.agenda_operations.Operations()
+
+    while True:
+        operations.menu()
+        client_choice = input("Select operation you need (1-8): ").strip()
+
+        if client_choice == '1':
+            operations.add_contact()
+        elif client_choice == '2':
+            operations.view_contacts()
+        elif client_choice == '3':
+            operations.edit_contacts()
+        elif client_choice == '4':
+            operations.deleting_contact()
+        elif client_choice == '5':
+            operations.search_contact()
+        elif client_choice == '6':
+            operations.save_contacts()
+            #print("Agenda saved successfully.")
+        elif client_choice == '7':
+            operations.load_contacts()
+            print("Agenda loaded successfully.")
+        elif client_choice == '8':
+            print("Exiting the program. Thank you!")
+            break
+        else:
+            print("Invalid option. Please select a number between 1 and 8.")
 
 if __name__ == "__main__":
     main()
