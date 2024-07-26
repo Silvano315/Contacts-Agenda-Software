8be1001bfd78a4f6044653e6fcd18d1dcefea612
Silvano Quarto <silvano.quarto@gmail.com>
2024-07-26 20:26:09 +0200

    constants added

---
+++ src/constants.py
@@ -0,0 +1,11 @@
+MAX_LENGTHS = {
+    'first name': 30,
+    'last name': 30,
+    'phone': 15,
+    'email': 50,
+    'group': 20,
+    'street': 50,
+    'city': 30,
+    'state': 20,
+    'note': 100
+}
